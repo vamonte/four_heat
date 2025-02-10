8f6d3378f4b9a0fad5dec0543a5c3c45ddc5e022
valentin monté <vmonte@mac.home>
2025-02-10 23:03:50 +0100

    f

--- custom_components/four_heat/config_flow.py
+++ custom_components/four_heat/config_flow.py
@@ -10,26 +10,28 @@ _LOGGER = logging.getLogger(__name__)
 class FourHeatLocalConfiGFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     MINOR_VERSION = 1
+    _user_inputs: dict = {}
 
     # name "async_step_user" is mandatory
     async def async_step_user(self, user_input: dict | None = None) -> FlowResult:
         _LOGGER.error(10)
         user_form = vol.Schema({
             vol.Required("ip"): str,
-            vol.Required("port", default="8080"): str,
+            vol.Required("port", default="80"): str,
         })
 
         if user_input is None:
             return self.async_show_form(step_id="user", data_schema=user_form)
         
-        _LOGGER.error(f"{user_input =}")
+        self._user_inputs.update(user_input)
         tcp_client = TCPClient(user_input["ip"], user_input["port"])
-        _LOGGER.error(0)
         stove = Stove(tcp_client)
-        _LOGGER.error(1)
         await stove.init_config()
-        _LOGGER.error(2)
-        self._user_inputs.update({"toto": "aa"})
+        self._user_inputs.update({"config": stove.config})
+        await self.step_complete_config()
 
     async def step_complete_config(self, user_input: dict | None = None) -> FlowResult:
-        print(self._user_inputs)
+        
+        fields = {}
+        for i, config in enumerate(self._user_inputs["config"].items()):
+            fields[vol.Required(f"config_{i}")] = str
